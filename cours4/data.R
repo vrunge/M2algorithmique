8ad7fc4cf82cfbfdfa8a54a5618542efb23d19d5
Vincent Runge <vincent.runge@univ-evry.fr>
2021-12-03 15:14:46 +0100

    Update data.R

--- cours4/data.R
+++ cours4/data.R
@@ -1,1 +1,23 @@
 
+
+studentProjectScore <- function(n, type = "random", pref = (1:n)^2)
+{
+  res <-  NULL
+  if(type == "pref")
+  {
+    res <- matrix(1:n,n,n, byrow = TRUE)
+    res <- t(apply(res,1,function(x) sample(x, prob = pref)))
+  }
+  if(type == "random")
+  {
+    res <- matrix(1:n,n,n, byrow = TRUE)
+    res <- t(apply(res,1,sample))
+  }
+  return(res)
+}
+
+
+studentProjectScore(10, type = "random")
+
+
+studentProjectScore(10, type = "pref")
